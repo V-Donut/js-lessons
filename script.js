efa669a7cfe3cae69016ffe17d59b2b747614db9
V-Donut <vikaevt123@gmail.com>
2022-01-27 21:10:33 +0300

    Домашняя работа №1

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-var myVar = 10;
+const title = 'заголовок';
+let screens = 0;
+let screenPrice = 25;
+let rollback = 'Hello, world!';
+let fullPrice = 555;
+const adaptive = true;
+
+alert('Тестовое сообщение');
+console.log('Первая домашняя');
