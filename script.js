04dc116f2a3440581a76a31682b1730d88876de7
V-Donut <vikaevt123@gmail.com>
2022-01-28 21:04:45 +0300

    Домашняя работа №2

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'Калькулятор вёрстки';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 15000;
+let rollback = 25;
+let fullPrice = 50000;
+let adaptive = true;
 
-alert('Тестовое сообщение');
-console.log('Первая домашняя');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log('Стоимость вёрстки экранов ' + screenPrice + ' рублей');
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
+
+screens = screens.toLocaleLowerCase();
+let screensArray = screens.split(', ');
+console.log(screensArray);
+
+let rollbackPercent = fullPrice * (rollback / 100);
+console.log(rollbackPercent);
