1b5786940fa86f2ec52d8462b6b31459a20118ac
V-Donut <vikaevt123@gmail.com>
2022-01-31 22:20:55 +0300

    Домашняя работа №3

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let title = 'Калькулятор вёрстки';
 let screens = 'Простые, Сложные, Интерактивные';
 let screenPrice = 15000;
@@ -18,5 +20,32 @@ screens = screens.toLocaleLowerCase();
 let screensArray = screens.split(', ');
 console.log(screensArray);
 
-let rollbackPercent = fullPrice * (rollback / 100);
-console.log(rollbackPercent);
+let rollbackSum = fullPrice * (rollback / 100);
+console.log(rollbackSum);
+
+
+title = prompt('Как называется ваш проект?');
+screens = prompt('Какие типы экранов нужно разработать?');
+screenPrice = +prompt('Сколько будет стоить данная работа?');
+
+adaptive = (prompt('Нужен ли адаптив на сайте?') === 'Да') ? true : false;
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = +prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = +prompt('Сколько это будет стоить?');
+
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+let servicePercentPrice = Math.ceil(fullPrice - rollbackSum);
+console.log(servicePercentPrice);
+
+if (fullPrice >= 30000) {
+  console.log('Даем скидку в 10%');
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+  console.log('Даем скидку в 5%');
+} else if (fullPrice >= 0 && fullPrice < 15000) {
+  console.log('Скидка не предусмотрена');
+} else {
+  console.log('Что-то пошло не так');
+}
