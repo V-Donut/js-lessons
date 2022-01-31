80aa0cd3151d1c0da26203ec81c96c1a9779536c
V-Donut <vikaevt123@gmail.com>
2022-02-01 00:04:21 +0300

    Внесла правки к д.з. №3

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 'use strict';
 
-let title = 'Калькулятор вёрстки';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 15000;
+let title = prompt('Как называется ваш проект?');
+let screens = prompt('Какие типы экранов нужно разработать?');
+let screenPrice = +prompt('Сколько будет стоить данная работа?');
 let rollback = 25;
-let fullPrice = 50000;
-let adaptive = true;
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = +prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = +prompt('Сколько это будет стоить?');
+
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
 console.log(typeof title);
 console.log(typeof fullPrice);
@@ -22,20 +28,6 @@ console.log(screensArray);
 
 let rollbackSum = fullPrice * (rollback / 100);
 console.log(rollbackSum);
-
-
-title = prompt('Как называется ваш проект?');
-screens = prompt('Какие типы экранов нужно разработать?');
-screenPrice = +prompt('Сколько будет стоить данная работа?');
-
-adaptive = (prompt('Нужен ли адаптив на сайте?') === 'Да') ? true : false;
-
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice1 = +prompt('Сколько это будет стоить?');
-let service2 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice2 = +prompt('Сколько это будет стоить?');
-
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
 let servicePercentPrice = Math.ceil(fullPrice - rollbackSum);
 console.log(servicePercentPrice);
