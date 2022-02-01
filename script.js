ff2bcb1807baa335083c19ee023dc2e448ef47f7
V-Donut <vikaevt123@gmail.com>
2022-02-01 19:52:49 +0300

    Домашняя работа №1

--- script.js
+++ script.js
@@ -1,43 +1,9 @@
-'use strict';
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
 
-let title = prompt('Как называется ваш проект?', 'Калькулятор верстки');
-let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-let screenPrice = +prompt('Сколько будет стоить данная работа?','10000');
-let rollback = 25;
-let adaptive = confirm('Нужен ли адаптив на сайте?');
-
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice1 = +prompt('Сколько это будет стоить?');
-let service2 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice2 = +prompt('Сколько это будет стоить?');
-
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-
-console.log(screens.length);
-
-console.log('Стоимость вёрстки экранов ' + screenPrice + ' рублей');
-console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
-
-screens = screens.toLocaleLowerCase();
-let screensArray = screens.split(', ');
-console.log(screensArray);
-
-let rollbackSum = fullPrice * (rollback / 100);
-console.log(rollbackSum);
-
-let servicePercentPrice = Math.ceil(fullPrice - rollbackSum);
-console.log(servicePercentPrice);
-
-if (fullPrice >= 30000) {
-  console.log('Даем скидку в 10%');
-} else if (fullPrice >= 15000 && fullPrice < 30000) {
-  console.log('Даем скидку в 5%');
-} else if (fullPrice >= 0 && fullPrice < 15000) {
-  console.log('Скидка не предусмотрена');
-} else {
-  console.log('Что-то пошло не так');
-}
+alert('Тестовое сообщение');
+console.log('Первая домашняя');
