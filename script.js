ada5d9320a96c775af8be38708e4c8d4c594fa14
V-Donut <vikaevt123@gmail.com>
2022-02-03 18:37:43 +0300

    Правки по д.з. №5

--- script.js
+++ script.js
@@ -10,20 +10,31 @@ let fullPrice;
 let servicePercentPrice;
 let service1;
 let service2;
-let servicePrice;
 
 const isNumber = function (num) {
-  return !isNaN(parseFloat(num)) && isFinite(num);
+  return !isNaN(parseFloat(num)) && isFinite(num) && num.toString().indexOf(' ') === -1;
+};
+
+const isString = function (str) {
+  if (!!!parseInt(str) == false || str == null) {
+    return false;
+  } else {
+    return str.trim();
+  }
+};
+
+const getUserAnswer = function (message, numb, check) {
+    let n;
+    do {
+        n = prompt(message, numb);
+    } while(!check(n));
+    return n.trim();
 };
 
 const asking = function () {
-  title = prompt('Как называется ваш проект?', 'Калькулятор верстки');
-  screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-
-  do {
-    screenPrice = prompt('Сколько будет стоить данная работа?');
-  } while (!isNumber(screenPrice));
-
+  title = getUserAnswer('Как называется ваш проект?', 'Калькулятор верстки', isString);
+  screens = getUserAnswer('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные', isString);
+  screenPrice = +getUserAnswer('Сколько будет стоить данная работа?', '15000', isNumber);
   adaptive = confirm('Нужен ли адаптив на сайте?');
 };
 
@@ -32,16 +43,11 @@ const getAllServicePrices = function () {
 
   for (let i = 0; i < 2; i++) {
     if (i === 0) {
-      service1 = prompt('Какой дополнительный тип услуги нужен?');
+      service1 = getUserAnswer('Какой дополнительный тип услуги нужен?', 'Метрика', isString);
     } else if (i === 1) {
-      service2 = prompt('Какой дополнительный тип услуги нужен?');
+      service2 = getUserAnswer('Какой дополнительный тип услуги нужен?', 'Отправка форм', isString);
     }
-
-    do {
-      servicePrice = prompt('Сколько это будет стоить?');
-    } while (!isNumber(servicePrice));
-
-    sum += parseFloat(servicePrice);
+    sum += +getUserAnswer('Сколько это будет стоить?', '1000', isNumber);
   }
 
   return sum;
