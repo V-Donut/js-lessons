b66539e064411867d273fb5eeed6438bb3306c6b
V-Donut <vikaevt123@gmail.com>
2022-02-09 00:04:12 +0300

    Внесла правки

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const title = document.getElementsByTagName('h1')[0].textContent;
-const buttons = document.getElementsByClassName('handler_btn');
-const calculation = buttons[0];
-const reset = buttons[1];
+const title = document.getElementsByTagName('h1')[0];
+const calculation = document.getElementsByClassName('handler_btn')[0];
+const reset = document.getElementsByClassName('handler_btn')[1];
 const screenPlus = document.querySelector('.screen-btn');
 
 const percentItems = document.querySelectorAll('.other-items.percent');
@@ -12,12 +11,11 @@ const numberItems = document.querySelectorAll('.other-items.number');
 const range = document.querySelector('.rollback [type="range"]');
 const rangeValue = document.querySelector('.rollback .range-value');
 
-const totalInputs = document.getElementsByClassName('total-input');
-const total = totalInputs[0];
-const totalCount = totalInputs[1];
-const totalCountOther = totalInputs[2];
-const totalFullCount = totalInputs[3];
-const totalCountRollback = totalInputs[4];
+const total = document.getElementsByClassName('total-input')[0];
+const totalCount = document.getElementsByClassName('total-input')[1];
+const totalCountOther = document.getElementsByClassName('total-input')[2];
+const totalFullCount = document.getElementsByClassName('total-input')[3];
+const totalCountRollback = document.getElementsByClassName('total-input')[4];
 
 let screens = document.querySelectorAll('.screen');
 
